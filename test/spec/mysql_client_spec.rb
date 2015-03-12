a1e09ddbd0c6a13d55257b7d791de471cb363787
Ignasi Fosch <natx@y10k.ws>
2015-03-12 19:02:17 +0100

    Adds ChefSpec tests for mysql_client installation

---
+++ test/spec/mysql_client_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'wp-cookbook::default' do
+  context 'when usin chef-solo' do
+    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    it 'installs mysql_client' do
+      expect(chef_run).to create_mysql_client('default')
+    end
+  end
+  context 'when usin chef-server' do
+    let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+    it 'installs mysql_client' do
+      expect(chef_run).to create_mysql_client('default')
+    end
+  end
+end
