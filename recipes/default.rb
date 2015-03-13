50106049cdcd1cea293f80f75a7d96dc5f3516c7
Ignasi Fosch <natx@y10k.ws>
2015-03-13 19:05:38 +0100

    Satisfies CentOS fix, so runs without apt

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,5 +24,5 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-include_recipe 'apt'
+include_recipe 'apt' if %w(ubuntu debian).include? node['platform']
 mysql_client 'default'
