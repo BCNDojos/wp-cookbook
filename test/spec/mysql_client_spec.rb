3690de29bc66b50b93c7d7ba14ebc0c0390e8c13
Ignasi Fosch <natx@y10k.ws>
2015-03-13 12:30:42 +0100

    Sets up everything to start testing with Precise and chef-solo

--- test/spec/mysql_client_spec.rb
+++ test/spec/mysql_client_spec.rb
@@ -1,4 +1,9 @@
 require './test/spec/spec_helper.rb'
 
 describe 'wp-cookbook::default' do
+  context 'on Precise with chef-solo' do
+    let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    it 'installs mysql client' do
+    end
+  end
 end
--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'chefspec'
 
-RSpec.configure do |conf|
+RSpec.configure do |config|
   # Specify cookbook path for Chef
   current_path = File.expand_path(File.dirname(__FILE__))
-  conf.cookbook_path = File.dirname(current_path + '/../../..')
+  config.cookbook_path = File.dirname(current_path + '/../../..')
+  # Let's suppose Ubuntu 12.04 by default
+  config.platform = 'ubuntu'
+  config.version = '12.04'
 end
