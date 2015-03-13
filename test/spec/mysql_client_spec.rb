91755c8d28f9478c2230851ddfea5a4f7c97ec8f
Ignasi Fosch <natx@y10k.ws>
2015-03-13 19:05:04 +0100

    Adds CentOS specific test

--- test/spec/mysql_client_spec.rb
+++ test/spec/mysql_client_spec.rb
@@ -8,4 +8,16 @@ describe 'wp-cookbook::default' do
       expect(chef_run).to create_mysql_client('default')
     end
   end
+  context 'on CentOS 6 with chef-solo' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(
+        platform: 'centos',
+        version: '6.4'
+      ).converge(described_recipe)
+    end
+    it 'installs mysql client' do
+      expect(chef_run).not_to include_recipe('apt')
+      expect(chef_run).to create_mysql_client('default')
+    end
+  end
 end
